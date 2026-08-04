9d34f738d66855cfae832e062e6b825388c170b1
orbitalbgmi-cpu <orbitalbgmi@gmail.com>
2026-08-04 08:17:29 +0530

    Create PluginsView.xaml.cs

---
+++ .github/workflows/Views/PluginsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RUNE.Views
+{
+    public partial class PluginsView : UserControl
+    {
+        public PluginsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
