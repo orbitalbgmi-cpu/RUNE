fda546b39c3abd3a6856399c9f820191732cc923
orbitalbgmi-cpu <orbitalbgmi@gmail.com>
2026-08-04 08:14:51 +0530

    Create SettingsView.xaml.cs

---
+++ .github/workflows/Views/SettingsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RUNE.Views
+{
+    public partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
