74bca391ec35238e335fc3f7bd1e5f4928c23a5e
orbitalbgmi-cpu <orbitalbgmi@gmail.com>
2026-08-06 11:02:15 +0530

    Update PluginsView.xaml.cs

--- Views/PluginsView.xaml.cs
+++ Views/PluginsView.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RUNE.Views
 {
@@ -7,6 +9,50 @@ namespace RUNE.Views
         public PluginsView()
         {
             InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                WebSearchToggle.IsChecked = App.Config.IsModuleEnabled("web-search");
+                RUNE.WebSearchModule.ActivityLogged += OnActivityLogged;
+                RefreshLogPlaceholder();
+            };
+            Unloaded += (s, e) => RUNE.WebSearchModule.ActivityLogged -= OnActivityLogged;
+        }
+
+        private void WebSearchToggle_Changed(object sender, RoutedEventArgs e)
+        {
+            App.Config.SetModuleEnabled("web-search", WebSearchToggle.IsChecked == true);
+        }
+
+        private void RefreshLogPlaceholder()
+        {
+            if (ActivityLog.Children.Count == 0)
+            {
+                ActivityLog.Children.Add(new TextBlock
+                {
+                    Text = "No search activity yet.",
+                    Foreground = (Brush)FindResource("SecondaryTextBrush"),
+                    FontStyle = FontStyles.Italic
+                });
+            }
+        }
+
+        private void OnActivityLogged(string entry)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (ActivityLog.Children.Count == 1 && ActivityLog.Children[0] is TextBlock tb && tb.FontStyle == FontStyles.Italic)
+                {
+                    ActivityLog.Children.Clear();
+                }
+
+                ActivityLog.Children.Add(new TextBlock
+                {
+                    Text = entry,
+                    Foreground = (Brush)FindResource("PrimaryTextBrush"),
+                    TextWrapping = TextWrapping.Wrap,
+                    Margin = new Thickness(0, 0, 0, 4)
+                });
+            });
         }
     }
 }
